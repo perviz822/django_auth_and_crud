758d57afeaa567f0a8813f3ef1c4ed4bd3b36e2f
parviz <perviz822@gmail.com>
2022-05-02 16:01:52 +0400

    Admin page customized using UserAdminConfig

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
+from backend.models import NewUser
+from django.contrib.auth.admin import UserAdmin
 from products.models import Products
 from backend.models import NewUser
 admin.site.register(Products)
-admin.site.register(NewUser)
+
+
+
+
+
+class UserAdminConfig(UserAdmin):
+    ordering=('user_name',)
+    list_display=('email','user_name','first_name','is_active','is_staff','is_superuser')
+    fieldsets=(
+        (None,{'fields':('email','user_name','first_name')}),
+        ('Permissions',{'fields':('is_staff','is_active','is_superuser')}),
+        ('Personal',{'fields':('about',)}),
+
+    )
+
+admin.site.register(NewUser,UserAdminConfig)
+# Register your models here.
+
 
 
 # Register your models here.
--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from backend.models import NewUser
+from django.contrib.auth.admin import UserAdmin
+
+
+class UserAdminConfig(UserAdmin):
+    ordering=('-start_date',)
+    list_display=('email','user_name','first_name','is_active','is_staff','is_superuser')
+
 
 # Register your models here.
